4d8e9db14b2ea1b694b62ad5892b1e7f5bfb54ab
Ashot <ashotoharutyunyan2002@gmail.com>
2021-04-29 13:15:51 +0400

    commit

---
+++ test.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class test {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        if (number >= 0){
+        }
+        else{
+            number*=-1;
+        }
+        System.out.println(number);
+
+    }
+}
